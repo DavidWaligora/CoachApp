1bd0714c625fd16a21626ce7f668dbae0948d5a7
David Waligora <davidwaligora@hotmail.com>
2025-07-04 17:04:29 +0200

    fixed unnecessary code

--- CoachApp/CoachApp.Services/UserClientData/UserClientDataServices.cs
+++ CoachApp/CoachApp.Services/UserClientData/UserClientDataServices.cs
@@ -5,24 +5,6 @@ namespace CoachApp.Services.UserClientData;
 
 public class UserClientDataServices(CoachAppContext context)
 {
-    public async Task<string?> GetUserNameByUserID(int userId)
-    {
-        string? user = await context.UserClient
-            .Where(x => x.UserID == userId)
-            .Select(x => x.User.UserName)
-            .FirstOrDefaultAsync();
-
-        return user ?? string.Empty;
-    }
-    public async Task<string?> GetUserNameBClientID(int clientId)
-    {
-        string? client = await context.UserClient
-            .Where(x => x.ClientID == clientId)
-            .Select(x => x.User.UserName)
-            .FirstOrDefaultAsync();
-
-        return client ?? string.Empty;
-    }
     public async Task<List<string?>> GetCoachesWhereIsClientAsync(int clientId)
     {
         List<string?> result = await context.UserClient
